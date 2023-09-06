ab06725656394ac76d627b56dc379f98a046c489
abigail-143 <abigailtoh1403@gmail.com>
2023-09-06 17:14:17 +0800

    add auth to purchase orders

--- backend/src/routers/purchaseOrder.ts
+++ backend/src/routers/purchaseOrder.ts
@@ -11,24 +11,28 @@ import {
   checkUpdatePurchaseOrderInput,
 } from "../validators/inputValidators";
 import { inputValidation } from "../middlewares/inputValidatorsCheck";
+import { authManager } from "../middlewares/authUsers";
 
 const router = express.Router();
 
-router.get("/all", getAllPurchaseOrders);
+router.get("/all", authManager, getAllPurchaseOrders);
 router.post(
   "/:poID",
+  authManager,
   checkPurchaseOrderIDParams,
   inputValidation,
   getOnePurchaseOrder
 );
 router.put(
   "/new",
+  authManager,
   checkAddNewPurchaseOrderInput,
   inputValidation,
   addNewPurchaseOrder
 );
 router.patch(
   "/:poID",
+  authManager,
   checkPurchaseOrderIDParams,
   checkUpdatePurchaseOrderInput,
   inputValidation,
