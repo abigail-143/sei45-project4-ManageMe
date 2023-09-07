80dd742a359b59c7e3239ee2ccf1acf33060626d
abigail-143 <abigailtoh1403@gmail.com>
2023-09-07 17:45:47 +0800

    component for manager navbar created + css module

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import styles from "./NavBar.module.css";
 
 export const NavBar: React.FC = () => {
-  return <div>navigation bar</div>;
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.companyLogo}>
+        <img className={styles.companyImg} src="/simulation.png"></img>
+        <p className={styles.companyName}>ManageMe</p>
+      </div>
+
+      <ul className={styles.navlinks}>
+        <li>
+          <a href="#">Dashboard</a>
+        </li>
+        <li>
+          <a href="#">Store Delivery</a>
+        </li>
+        <li>
+          <a href="#">Purchase Orders</a>
+        </li>
+        <li>
+          <a href="#">Stock Levels</a>
+        </li>
+        <li>
+          <a href="#">Reports</a>
+        </li>
+        <li>
+          <a href="#">Register User</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
-// if manager <SearchBar/> 2x <OrderButton> <img></img>
-// if staff <SearchBar /> 1x <OrderButton> <img></img>
+// if manager 5-6 links
+// if staff 3-4 links
