5b65b81803da8f078ce2fff423410db45b2e1427
abigail-143 <abigailtoh1403@gmail.com>
2023-09-09 12:01:48 +0800

    update css module for registeruser component

--- frontend/src/Components/ManagerOnly/RegisterUser.tsx
+++ frontend/src/Components/ManagerOnly/RegisterUser.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./RegisterUser.module.css";
 
 interface props {
   role: string;
@@ -16,76 +17,100 @@ export const RegisterUser: React.FC<props> = (props) => {
   // map out the data from backend to get the table rows
   const staff = staffList.map((user, index) => {
     return (
-      <div key={index} className="listBodyRows">
-        <p className="column username">{user.username}</p>
-        <p className="column email">{user.email}</p>
-        <p className="column status">{user.userStatus}</p>
+      <div key={index} className={styles.listBodyRows}>
+        <p className={`${styles.column} ${styles.username}`}>{user.username}</p>
+        <p className={`${styles.column} ${styles.email}`}>{user.email}</p>
+        <p className={`${styles.column} ${styles.status}`}>{user.userStatus}</p>
       </div>
     );
   });
 
   return (
     <>
-      <div className="registerPage">
-        <div className="formSection">
-          <div className="registerForm">
-            <h1 className="register">Register New User</h1>
-            <div className="registerInput">
-              <label className="label">Username:</label>
-              <input className="input" placeholder="username"></input>
+      <div className={styles.registerPage}>
+        <div className={styles.formSection}>
+          <div className={styles.registerForm}>
+            <h1 className={styles.register}>Register New User</h1>
+            <div className={styles.registerInput}>
+              <label className={styles.label}>Username:</label>
+              <input className={styles.input} placeholder="username"></input>
             </div>
-            <div className="registerInput">
-              <label className="label">Email:</label>
-              <input className="input" placeholder="email"></input>
+            <div className={styles.registerInput}>
+              <label className={styles.label}>Email:</label>
+              <input className={styles.input} placeholder="email"></input>
             </div>
-            <div className="registerInput">
-              <label className="label">Password:</label>
-              <input className="input" placeholder="password"></input>
+            <div className={styles.registerInput}>
+              <label className={styles.label}>Password:</label>
+              <input className={styles.input} placeholder="password"></input>
             </div>
-            <div className="registerInput">
-              <label className="label">Company:</label>
-              <input className="input" placeholder="company"></input>
+            <div className={styles.registerInput}>
+              <label className={styles.label}>Company:</label>
+              <input className={styles.input} placeholder="company"></input>
             </div>
-            <div className="registerInput">
-              <label className="label">Status:</label>
-              <input className="input" placeholder="status"></input>
+            <div className={styles.registerInput}>
+              <label className={styles.label}>Status:</label>
+              <input className={styles.input} placeholder="status"></input>
             </div>
-            <div className="registerInput">
-              <label className="label">Account Type:</label>
-              <input className="input" placeholder="active"></input>
+            <div className={styles.registerInput}>
+              <label className={styles.label}>Account Type:</label>
+              <input className={styles.input} placeholder="active"></input>
             </div>
-            <div className="btnDiv">
-              <button className="registerBtn">Register</button>
+            <div className={styles.btnDiv}>
+              <button className={styles.registerBtn}>Register</button>
             </div>
           </div>
         </div>
-        <div className="listSection">
+        <div className={styles.listSection}>
           {/* this is the manager list */}
-          <div className="userList">
-            <h2 className="listName">Manager Accounts</h2>
-            <div className="listHeaders">
-              <div className="listBodyRows">
-                <p className="column header username">Username</p>
-                <p className="column header email">Email</p>
-                <p className="column header status">User Status</p>
+          <div className={styles.userList}>
+            <h2 className={styles.listName}>Manager Accounts</h2>
+            <div className={styles.listHeaders}>
+              <div className={styles.listBodyRows}>
+                <p
+                  className={`${styles.column} ${styles.header} ${styles.username}`}
+                >
+                  Username
+                </p>
+                <p
+                  className={`${styles.column} ${styles.header} ${styles.email}`}
+                >
+                  Email
+                </p>
+                <p
+                  className={`${styles.column} ${styles.header} ${styles.status}`}
+                >
+                  User Status
+                </p>
               </div>
             </div>
-            <div className="listBody">
+            <div className={styles.listBody}>
               {staff}
               {staff}
             </div>
           </div>
           {/* this is the staff list */}
-          <div className="userList">
-            <h2 className="listName">Staff Accounts</h2>
-            <div className="listHeaders">
-              <div className="listBodyRows">
-                <p className="column header username">Username</p>
-                <p className="column header email">Email</p>
-                <p className="column header status">User Status</p>
+          <div className={styles.userList}>
+            <h2 className={styles.listName}>Staff Accounts</h2>
+            <div className={styles.listHeaders}>
+              <div className={styles.listBodyRows}>
+                <p
+                  className={`${styles.column} ${styles.header} ${styles.username}`}
+                >
+                  Username
+                </p>
+                <p
+                  className={`${styles.column} ${styles.header} ${styles.email}`}
+                >
+                  Email
+                </p>
+                <p
+                  className={`${styles.column} ${styles.header} ${styles.status}`}
+                >
+                  User Status
+                </p>
               </div>
             </div>
-            <div className="listBody">
+            <div className={styles.listBody}>
               {staff}
               {staff}
             </div>
