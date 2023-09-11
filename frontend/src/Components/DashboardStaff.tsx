65751bb52de4e42ac2c657aa3229d15affb3304b
abigail-143 <abigailtoh1403@gmail.com>
2023-09-12 00:20:12 +0800

    pull DO data from backend and map to staffdashboard component

--- frontend/src/Components/DashboardStaff.tsx
+++ frontend/src/Components/DashboardStaff.tsx
@@ -1,7 +1,39 @@
-import React from "react";
-import styles from "./DashboardStaff.module.css"
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./DashboardStaff.module.css";
+import { useFetch } from "../hooks/useFetch";
+import UserContext from "../context/user";
 
 export const DashboardStaff: React.FC = () => {
+  const fetchData = useFetch();
+  const context = useContext(UserContext);
+  const [doData, setDOData] = useState<
+    {
+      delivery_id: number;
+      username: string;
+      delivery_placed_date: Date;
+      to_deliver_date: Date;
+      delivered_date: Date;
+      completed: boolean;
+    }[]
+  >([]);
+
+  const pullAllDO = async () => {
+    const res = await fetchData(
+      "/do/all",
+      "GET",
+      undefined,
+      context?.accessToken
+    );
+
+    if (res.ok) {
+      console.log("DO ok");
+      setDOData(res.data);
+    } else {
+      console.log("fetch DO error");
+      console.log(res.data);
+    }
+  };
+
   // pull data from PO Table
   const poTableRows: {
     poID: number;
@@ -97,6 +129,11 @@ export const DashboardStaff: React.FC = () => {
       </div>
     );
   });
+
+  useEffect(() => {
+    pullAllDO();
+  }, []);
+  
   return (
     <div className={styles.dashboardPage}>
       <div className={styles.first}>
@@ -110,9 +147,22 @@ export const DashboardStaff: React.FC = () => {
               <p>To Deliver Date</p>
             </div>
             <div className={styles.doTableRowsDiv}>
+              {doData.map((item, index) => {
+                const orderDate = String(item.delivery_placed_date).split(
+                  "T"
+                )[0];
+                return (
+                  <div key={index} className={styles.doTableRows}>
+                    <p>Order {item.delivery_id}</p>
+                    <p>{item.username}</p>
+                    <p>{orderDate}</p>
+                    <p>{item.completed ? "Completed" : "Pending"}</p>
+                  </div>
+                );
+              })}
+              {/* {doRow}
               {doRow}
-              {doRow}
-              {doRow}
+              {doRow} */}
             </div>
           </div>
         </div>
