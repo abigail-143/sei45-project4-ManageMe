fbdf7827b9ccd59a45e3e9f3cc643cd0bebcee3e
abigail-143 <abigailtoh1403@gmail.com>
2023-09-12 14:13:09 +0800

    update productwarehouse and productstore to be able to add products

--- frontend/src/Components/ProductStore.tsx
+++ frontend/src/Components/ProductStore.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import styles from "./ProductStore.module.css";
 import { useFetch } from "../hooks/useFetch";
 import UserContext from "../context/user";
@@ -22,6 +22,8 @@ export const ProductStore: React.FC<props> = (props) => {
       in_use: boolean;
     }[]
   >([]);
+  const productIDRef = useRef<HTMLInputElement | null>(null);
+  const storeQuantityRef = useRef<HTMLInputElement | null>(null);
 
   // GET all store products from store table
   const pullStoreProducts = async () => {
@@ -42,6 +44,32 @@ export const ProductStore: React.FC<props> = (props) => {
     }
   };
 
+  // ADD new product to store
+  const addNewProductToStore = async () => {
+    if (productIDRef.current && storeQuantityRef.current) {
+      const res = await fetchData(
+        "/store/add",
+        "PUT",
+        {
+          productID: productIDRef.current.value,
+          storeQuantity: storeQuantityRef.current.value,
+        },
+        context?.accessToken
+      );
+
+      if (res.ok) {
+        console.log("add product to store successful");
+        console.log(res.data);
+        pullStoreProducts();
+        productIDRef.current.value = "";
+        storeQuantityRef.current.value = "";
+      } else {
+        console.log("add product to store error");
+        console.log(res.data);
+      }
+    }
+  };
+
   useEffect(() => {
     pullStoreProducts();
   }, []);
@@ -53,16 +81,27 @@ export const ProductStore: React.FC<props> = (props) => {
           <h1 className={styles.formTitle}>Add New Product To Store</h1>
           <div className={styles.formInput}>
             <label className={styles.label}>Product ID:</label>
-            <input className={styles.input} placeholder="input"></input>
+            <input
+              ref={productIDRef}
+              className={styles.input}
+              placeholder="input"
+            ></input>
           </div>
           <div className={styles.formInput}>
             <label className={styles.label}>Quantity</label>
-            <input className={styles.input} placeholder="input"></input>
+            <input
+              ref={storeQuantityRef}
+              className={styles.input}
+              placeholder="input"
+            ></input>
           </div>
           <button
             className={`${styles.addBtn} ${
               props.role == "Manager" ? styles.blue : styles.green
             }`}
+            onClick={() => {
+              addNewProductToStore();
+            }}
           >
             Add To Store
           </button>
@@ -79,35 +118,38 @@ export const ProductStore: React.FC<props> = (props) => {
               <p className={`${styles.headerInput} ${styles.middle}`}>
                 Product Description
               </p>
-              <p className={`${styles.headerInput} ${styles.middle}`}>UOM</p>
               <p className={`${styles.headerInput} ${styles.middle}`}>
-                Piece per UOM
+                Store Quantity
               </p>
+              <p className={`${styles.headerInput} ${styles.middle}`}>UOM</p>
               <p className={`${styles.headerInput} ${styles.last}`}>In Use</p>
             </div>
           </div>
           <div className={styles.listBody}>
-            {storeProduct.map((item, index) => {
-              return (
-                <div key={index} className={styles.listRows}>
-                  <p className={`${styles.bodyInput} ${styles.first}`}>
-                    {item.product_id}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    {item.product_description}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    {item.unit_of_measurement}{" "}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    {item.piece_per_uom}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.last}`}>
-                    {item.in_use ? "in use" : "not in use"}
-                  </p>
-                </div>
-              );
-            })}
+            {storeProduct
+              .slice()
+              .reverse()
+              .map((item, index) => {
+                return (
+                  <div key={index} className={styles.listRows}>
+                    <p className={`${styles.bodyInput} ${styles.first}`}>
+                      {item.product_id}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      {item.product_description}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      {item.store_quantity}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      {item.unit_of_measurement}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.last}`}>
+                      {item.in_use ? "in use" : "not in use"}
+                    </p>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
--- frontend/src/Components/ProductWarehouse.tsx
+++ frontend/src/Components/ProductWarehouse.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import styles from "./ProductWarehouse.module.css";
 import { useFetch } from "../hooks/useFetch";
 import UserContext from "../context/user";
@@ -20,7 +20,10 @@ export const ProductWarehouse: React.FC = () => {
       supplier_leadtime: number;
     }[]
   >([]);
+  const productIDRef = useRef<HTMLInputElement | null>(null);
+  const warehouseQuantityRef = useRef<HTMLInputElement | null>(null);
 
+  // GET all products in warehouse
   const pullWarehouseProducts = async () => {
     const res = await fetchData(
       "/warehouse/all",
@@ -39,6 +42,32 @@ export const ProductWarehouse: React.FC = () => {
     }
   };
 
+  // ADD new product to warehouse
+  const addNewProductToWarehouse = async () => {
+    if (productIDRef.current && warehouseQuantityRef.current) {
+      const res = await fetchData(
+        "/warehouse/add",
+        "PUT",
+        {
+          productID: productIDRef.current.value,
+          warehouseQuantity: warehouseQuantityRef.current.value,
+        },
+        context?.accessToken
+      );
+
+      if (res.ok) {
+        console.log("add product to warehouse successful");
+        console.log(res.data);
+        pullWarehouseProducts();
+        productIDRef.current.value = "";
+        warehouseQuantityRef.current.value = "";
+      } else {
+        console.log("add product to warehouse error");
+        console.log(res.data);
+      }
+    }
+  };
+
   useEffect(() => {
     pullWarehouseProducts();
   }, []);
@@ -50,13 +79,28 @@ export const ProductWarehouse: React.FC = () => {
           <h1 className={styles.formTitle}>Add New Product To Warehouse</h1>
           <div className={styles.formInput}>
             <label className={styles.label}>Product ID:</label>
-            <input className={styles.input} placeholder="input"></input>
+            <input
+              ref={productIDRef}
+              className={styles.input}
+              placeholder="input"
+            ></input>
           </div>
           <div className={styles.formInput}>
             <label className={styles.label}>Quantity</label>
-            <input className={styles.input} placeholder="input"></input>
+            <input
+              ref={warehouseQuantityRef}
+              className={styles.input}
+              placeholder="input"
+            ></input>
           </div>
-          <button className={styles.addBtn}>Add To Warehouse</button>
+          <button
+            className={styles.addBtn}
+            onClick={() => {
+              addNewProductToWarehouse();
+            }}
+          >
+            Add To Warehouse
+          </button>
         </div>
       </div>
       <div className={styles.secondRow}>
@@ -70,10 +114,10 @@ export const ProductWarehouse: React.FC = () => {
               <p className={`${styles.headerInput} ${styles.middle}`}>
                 Product Description
               </p>
-              <p className={`${styles.headerInput} ${styles.middle}`}>UOM</p>
               <p className={`${styles.headerInput} ${styles.middle}`}>
-                Piece per UOM
+                Warehouse Quantity
               </p>
+              <p className={`${styles.headerInput} ${styles.middle}`}>UOM</p>
               <p className={`${styles.headerInput} ${styles.middle}`}>
                 Cost per UOM
               </p>
@@ -87,36 +131,39 @@ export const ProductWarehouse: React.FC = () => {
             </div>
           </div>
           <div className={styles.listBody}>
-            {warehouseProduct.map((item, index) => {
-              return (
-                <div key={index} className={styles.listRows}>
-                  <p className={`${styles.bodyInput} ${styles.first}`}>
-                    {item.product_id}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    {item.product_description}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    {item.unit_of_measurement}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    {item.piece_per_uom}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    ${item.cost_per_uom}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    {item.supplier}
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.middle}`}>
-                    {item.supplier_leadtime} days
-                  </p>
-                  <p className={`${styles.bodyInput} ${styles.last}`}>
-                    {item.in_use ? "in use" : "not in use"}
-                  </p>
-                </div>
-              );
-            })}
+            {warehouseProduct
+              .slice()
+              .reverse()
+              .map((item, index) => {
+                return (
+                  <div key={index} className={styles.listRows}>
+                    <p className={`${styles.bodyInput} ${styles.first}`}>
+                      {item.product_id}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      {item.product_description}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      {item.warehouse_quantity}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      {item.unit_of_measurement}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      ${item.cost_per_uom}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      {item.supplier}
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.middle}`}>
+                      {item.supplier_leadtime} days
+                    </p>
+                    <p className={`${styles.bodyInput} ${styles.last}`}>
+                      {item.in_use ? "in use" : "not in use"}
+                    </p>
+                  </div>
+                );
+              })}
             {/* <div className={styles.listRows}>
               <p className={`${styles.bodyInput} ${styles.first}`}>
                 Product ID
