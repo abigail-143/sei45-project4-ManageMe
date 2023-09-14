9962b48d232feaa7bc6408e811c348ea52697c1e
abigail-143 <abigailtoh1403@gmail.com>
2023-09-14 16:32:05 +0800

    add table to stocklevelwarehouse

--- frontend/src/Components/StockLevelWarehouse.tsx
+++ frontend/src/Components/StockLevelWarehouse.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./StockLevelWarehouse.module.css";
 import { WarehouseStockChart } from "./Charts/WarehouseStockChart";
+import { POChart } from "./Charts/POChart";
 
 export const StockLevelWarehouse: React.FC = () => {
+  const [productID, setProductID] = useState<string>("SQA0001");
+
   return (
     <div className={styles.stockWarehouse}>
-      <div className={styles.tabRow}>
+      {/* build next time */}
+      {/* <div className={styles.tabRow}>
         <div className={styles.tabSection}>
           <button className={styles.tabBtn}>All Products</button>
         </div>
@@ -20,11 +24,29 @@ export const StockLevelWarehouse: React.FC = () => {
           <input className={styles.searchInput} placeholder="Product"></input>
           <button className={styles.searchBtn}>Find</button>
         </div>
-      </div>
-      <div className={styles.tableDisplay}>
+      </div> */}
+      <div className={`${styles.tableDisplay} ${styles.one}`}>
         <h1 className={styles.title}>Warehouse Stock Levels</h1>
         <div className={styles.table}>
           <WarehouseStockChart></WarehouseStockChart>
+        </div>
+      </div>
+      <div className={`${styles.tableDisplay} ${styles.two}`}>
+        <div className={styles.secondTableHeader}>
+          <h1 className={styles.title}>PO Orders</h1>
+          <div className={styles.searchBar}>
+            <input
+              placeholder="Find Product"
+              onChange={(event) => {
+                const value = event.target.value;
+                const upper = value.toUpperCase();
+                setProductID(upper);
+              }}
+            ></input>
+          </div>
+        </div>
+        <div className={styles.table}>
+          <POChart productID={productID}></POChart>
         </div>
       </div>
     </div>
