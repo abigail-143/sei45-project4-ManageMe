a71278346234d352a831ae0399f98454452aead0
abigail-143 <abigailtoh1403@gmail.com>
2023-09-14 16:30:46 +0800

    update stock charts to store in different states

--- frontend/src/Components/Charts/StoreStockChart.tsx
+++ frontend/src/Components/Charts/StoreStockChart.tsx
@@ -14,7 +14,7 @@ import {
 
 interface props {
   role: string;
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 export const StoreStockChart: React.FC<props> = (props) => {
@@ -30,17 +30,8 @@ export const StoreStockChart: React.FC<props> = (props) => {
   const context = useContext(UserContext);
 
   // storing the data
-  const [storeStockData, setStoreStockData] = useState<
-    {
-      store_id: number;
-      store_quantity: number;
-      product_id: string;
-      product_description: string;
-      unit_of_measurement: string;
-      piece_per_uom: number;
-      in_use: boolean;
-    }[]
-  >([]);
+  const [productID, setProductID] = useState([]);
+  const [productQuantity, setProductQuantity] = useState([]);
 
   // GET data from database
   const getStoreData = async () => {
@@ -54,27 +45,58 @@ export const StoreStockChart: React.FC<props> = (props) => {
     if (res.ok) {
       console.log("fetch data ok");
       console.log(res.data);
-      setStoreStockData(res.data);
+      // setStoreStockData(res.data);
+
+      const tempProductID = res.data.map(
+        (item: {
+          store_id: number;
+          store_quantity: number;
+          product_id: string;
+          product_description: string;
+          unit_of_measurement: string;
+          piece_per_uom: number;
+          in_use: boolean;
+        }) => {
+          return item.product_id;
+        }
+      );
+      setProductID(tempProductID);
+
+      const tempProductQuantity = res.data.map(
+        (item: {
+          store_id: number;
+          store_quantity: number;
+          product_id: string;
+          product_description: string;
+          unit_of_measurement: string;
+          piece_per_uom: number;
+          in_use: boolean;
+        }) => {
+          return item.store_quantity;
+        }
+      );
+      setProductQuantity(tempProductQuantity);
     } else {
       console.log("fetch data error");
       console.log(res.data);
     }
   };
 
-  const productIDs = storeStockData.map((item) => item.product_id);
-  const productQuantities = storeStockData.map((item) => item.store_quantity);
-
   const chartData = {
-    labels: productIDs,
+    labels: productID,
     datasets: [
       {
         label: "Store Quantities",
-        data: productQuantities,
-        backgroundColor: props.role == "Manager" ? "rgb(124, 157, 187)" : "rgb(180, 205, 147)", // Customize the color
-        borderColor:  props.role == "Manager" ? "rgb(124, 157, 187)" : "rgb(180, 205, 147)",
+        data: productQuantity,
+        backgroundColor:
+          props.role == "Manager" ? "rgb(124, 157, 187)" : "rgb(180, 205, 147)", // Customize the color
+        borderColor:
+          props.role == "Manager" ? "rgb(124, 157, 187)" : "rgb(180, 205, 147)",
         borderWidth: 1,
-        hoverBackgroundColor: props.role == "Manager" ? "rgb(37, 112, 179)" : "rgb(66, 121, 91)",
-        hoverBorderColor: props.role == "Manager" ? "rgb(37, 112, 179)" : "rgb(66, 121, 91)",
+        hoverBackgroundColor:
+          props.role == "Manager" ? "rgb(37, 112, 179)" : "rgb(66, 121, 91)",
+        hoverBorderColor:
+          props.role == "Manager" ? "rgb(37, 112, 179)" : "rgb(66, 121, 91)",
       },
     ],
   };
