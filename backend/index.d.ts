ba79d0a29c6e53ffb7f5aa7eca9add68f8035926
abigail-143 <abigailtoh1403@gmail.com>
2023-10-05 15:48:33 +0800

    include export statement

--- backend/index.d.ts
+++ backend/index.d.ts
@@ -1,13 +1,15 @@
-declare module 'bcrypt';
-declare module 'jsonwebtoken';
-declare module 'uuid';
+declare module "bcrypt";
+declare module "jsonwebtoken";
+declare module "uuid";
 
 import { JwtPayload } from "jsonwebtoken";
 
 declare global {
-    namespace Express {
-      interface Request {
-        decoded: string | JwtPayload;
-      }
+  namespace Express {
+    interface Request {
+      decoded: string | JwtPayload;
     }
   }
+}
+
+export {};
